d82f86243065b018ffe33989d4e7b4b150a9a928
Abhishek Sinha <abhisheksinha27071994@gmail.com>
2018-03-21 04:49:07 +0530

    added home page

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
-var express = require("express");
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var mongoose = require('mongoose');
+
+// creating routes
+var home = require('./routes/index');
+var menu = require('./routes/menu');
+
 var app = express();
- app.get('/',function(req,res){
+
+// view engine setup
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine' , 'pug');
+
+// middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routing the views
+app.use('/', home);
+app.use('/menu', menu);
+
+/* app.get('/',function(req,res){
 	 res.sendFile(__dirname + '/public/index.html');
  });
-app.listen(5000);
+ */
+app.listen(3000, function(err , result){
+	if(err){
+		colsole.log(err);
+	}
+	console.log('server started on port 3000');
+});
